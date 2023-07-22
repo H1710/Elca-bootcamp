57b67a1efd342490fe7c7d1afe0f793619f11cf1
H1710 <huyquac@gmail.com>
2023-07-22 13:09:13 +0700

    fix cors

--- spring-boot-server/src/main/java/com/example/springbootserver/controller/GroupController.java
+++ spring-boot-server/src/main/java/com/example/springbootserver/controller/GroupController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "ttps://elca-bootcamp-production.up.railway.app")
 public class GroupController {
     @Autowired
     private GroupService groupService;
